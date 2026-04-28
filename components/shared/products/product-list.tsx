7e3fae85597118e36ed2906786d926a48c6fb5cc
ashrafomid2025 <omidmashraf@gmail.com>
2026-04-28 21:48:14 +0430

    product list updated

--- components/shared/products/product-list.tsx
+++ components/shared/products/product-list.tsx
@@ -12,8 +12,8 @@ function ProductList({
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title ? title : "New Products"}</h2>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {productList.map((x: any) => (
-          <ProductCard key={x.id} product={x} />
+        {productList.map((y: any) => (
+          <ProductCard key={y.id} product={y} />
         ))}
       </div>
     </div>
