bc63731152a7c0af0bb1db705117ff6c27f518a0
ashrafomid2025 <omidmashraf@gmail.com>
2026-05-02 21:23:07 +0430

    product price component added

--- components/shared/products/product-price.tsx
+++ components/shared/products/product-price.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProductPrice({ price }: { price: string }) {
   const [int, float] = price.split(".");
   return (
-    <div className="text-xl">
+    <div className="text-2xl">
       <span className="text-xs align-super">AFN</span>
       {int}
       <span className="text-xs align-super">.{float}</span>
